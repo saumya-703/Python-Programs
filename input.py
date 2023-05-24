22e153a0e69741c31aaedbc324a482a2ebafd233
saumya <verma.saumya.703@gmail.com>
2023-05-24 19:44:09 +0530

    Taking input

---
+++ input.py
@@ -0,0 +1,11 @@
+#Taking the integer input find the sum
+spce= int(input("Entre the Number: "))
+space=int(input("Enter the second number: "))
+sum=space+spce
+print(sum)
+
+#Taking the string as input using the add operation
+s1=input("Enter the string: ")
+s2=input("Enter the string2: ")
+c=s1+s2
+print(c)
